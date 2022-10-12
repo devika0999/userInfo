8896a4365c750c1fcef6a7bd6a923907c991eaf5
devika0999 <krishnadevika445@gmail.com>
2022-10-12 10:57:15 +0530

    userInfo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
-
+import { InfoComponent } from './info/info.component';
+import { UserinfoComponent } from './userinfo/userinfo.component';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule} from '@angular/common/http'
+const appRoutes:Routes=[
+  {
+    path:"",component:UserinfoComponent
+  }
+]
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    InfoComponent,
+    UserinfoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
